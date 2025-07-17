54f37c8892d11336a3239744a3702ee692c1d9ba
LackFos <LackFosisOnline@gmail.com>
2025-07-17 10:36:07 +0700

    Initial Commit

---
+++ Mindful Breather Watch App/Mindful_BreatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mindful_BreatherApp.swift
+//  Mindful Breather Watch App
+//
+//  Created by Elvis on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Mindful_Breather_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
